6410e9f9d10ef6d6831048586259dc14e87afe97
arivanandhan <arivanandhan33@gmail.com>
2025-03-11 08:44:07 +0530

    Initial commit1

--- server.js
+++ server.js
@@ -78,6 +78,6 @@ app.get('/', (req, res) => {
   res.send('API is running...');
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
